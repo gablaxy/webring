5f76906794cec12e143655bf27fd0a147de98de6
gablaxy <32848524+gablaxy@users.noreply.github.com>
2023-04-19 11:46:30 +0200

    Update webring.js

--- webring.js
+++ webring.js
@@ -1,4 +1,4 @@
-const JSON_WEBRING = "https://gablaxy.github.io/webring.json";
+const JSON_WEBRING = "https://raw.githubusercontent.com/gablaxy/webring/main/webring.json";
 
 const template = document.createElement("template");
 template.innerHTML = `
